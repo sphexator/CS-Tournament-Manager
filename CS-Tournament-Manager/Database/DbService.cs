6e4af2d8580a5302fdbc5a4526c4c5e418aadf19
Kristoffer T <kristoffer.tunlid@gmail.com>
2023-05-07 23:14:19 +0200

    Added interface for dbcontext for better mocking

--- CS-Tournament-Manager/Database/DbService.cs
+++ CS-Tournament-Manager/Database/DbService.cs
@@ -1,17 +1,17 @@
 ﻿using CS_Tournament_Manager.Entities;
+using CS_Tournament_Manager.Entities.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace CS_Tournament_Manager.Database;
 
-public class DbService : DbContext
+public class DbService : DbContext, IDbService
 {
+    public DbService(DbContextOptions<DbService> options) : base(options) { }
+    
     public DbSet<Tournament> Tournaments { get; set; }
     public DbSet<Team> Teams { get; set; }
     public DbSet<Player> Players { get; set; }
     public DbSet<Match> Matches { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-        => base.OnConfiguring(optionsBuilder);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -36,4 +36,11 @@ public class DbService : DbContext
             x.Property(e => e.Id).ValueGeneratedOnAdd();
         });
     }
+    
+    public async Task<int> SaveChangesAsync(CancellationToken ctx = default) 
+        => await base.SaveChangesAsync(ctx);
+    public async Task<bool> EnsureDatabaseCreated(CancellationToken ctx = default) 
+        => await base.Database.EnsureCreatedAsync(ctx);
+    public async Task MigrateDatabase(CancellationToken ctx = default) 
+        => await base.Database.MigrateAsync(cancellationToken: ctx);
 }
---
+++ CS-Tournament-Manager/Entities/Interfaces/IDbService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CS_Tournament_Manager.Entities.Interfaces;
+
+public interface IDbService
+{
+    DbSet<Tournament> Tournaments { get; set; }
+    DbSet<Team> Teams { get; set; }
+    DbSet<Player> Players { get; set; }
+    DbSet<Match> Matches { get; set; }
+
+    /// <summary>
+    /// Saves changes in current context
+    /// </summary>
+    /// <returns></returns>
+    Task<int> SaveChangesAsync(CancellationToken ctx = default);
+
+    /// <summary>
+    /// Checks if the database is created
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> EnsureDatabaseCreated(CancellationToken ctx = default);
+
+    /// <summary>
+    /// Migrates pending migrations to the database
+    /// </summary>
+    /// <returns></returns>
+    Task MigrateDatabase(CancellationToken ctx = default);
+}
