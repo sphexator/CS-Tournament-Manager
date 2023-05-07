51a999b1c2d82c4e74033b88027f27d70ba61725
Kristoffer T <kristoffer.tunlid@gmail.com>
2023-05-07 23:17:11 +0200

    Better check on create tournament and return null if fail

--- CS-Tournament-Manager/Handlers/TournamentHandlers/CreateTournamentHandler.cs
+++ CS-Tournament-Manager/Handlers/TournamentHandlers/CreateTournamentHandler.cs
@@ -1,24 +1,24 @@
-﻿using CS_Tournament_Manager.Database;
-using CS_Tournament_Manager.Entities;
+﻿using CS_Tournament_Manager.Entities;
 using CS_Tournament_Manager.Entities.Interfaces;
 using MediatR;
 
 namespace CS_Tournament_Manager.Handlers.TournamentHandlers;
 
-public record CreateTournament() : IRequest<ITournament>;
+public record CreateTournament(Tournament Tournament) : IRequest<ITournament>;
 
 
 public class CreateTournamentHandler : IRequestHandler<CreateTournament, ITournament>
 {
-    private readonly DbService _dbService;
+    private readonly IDbService _dbService;
 
-    public CreateTournamentHandler(DbService dbService) => _dbService = dbService;
+    public CreateTournamentHandler(IDbService dbService) => _dbService = dbService;
 
     public async Task<ITournament> Handle(CreateTournament request, CancellationToken cancellationToken)
     {
-        var tournament = new Tournament{ Id = Guid.NewGuid() };
-        await _dbService.Tournaments.AddAsync(tournament, cancellationToken);
+        if(request.Tournament.Id == Guid.Empty) request.Tournament.Id = Guid.NewGuid();
+        if (request.Tournament.Name.Trim() is null or "") return null;
+        await _dbService.Tournaments.AddAsync(request.Tournament, cancellationToken);
         await _dbService.SaveChangesAsync(cancellationToken);
-        return tournament;
+        return request.Tournament;
     }
 }
